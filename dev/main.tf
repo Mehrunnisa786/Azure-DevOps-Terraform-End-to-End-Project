f1f6c5d4d2dd1fa067945470d666884a6da9a585
Mehrunnisa <mehrunnisaafrah@gmail.com>
2026-06-30 16:50:25 +0530

    Enhance main.tf with Key Vault and AKS modules
    Added Azure Key Vault and AKS resources with role assignments.

--- dev/main.tf
+++ dev/main.tf
@@ -2,27 +2,21 @@ resource "azurerm_resource_group" "rg1" {
   name     = var.rgname
   location = var.location
 }
-
 module "ServicePrincipal" {
   source                 = "../modules/ServicePrincipal"
   service_principal_name = var.service_principal_name
-
   depends_on = [
     azurerm_resource_group.rg1
   ]
 }
-
 resource "azurerm_role_assignment" "rolespn" {
-
   scope                = "/subscriptions/${var.SUB_ID}"
   role_definition_name = "Contributor"
   principal_id         = module.ServicePrincipal.service_principal_object_id
-
   depends_on = [
     module.ServicePrincipal
   ]
 }
-
 module "keyvault" {
   source                      = "../modules/keyvault"
   keyvault_name               = var.keyvault_name
@@ -31,10 +25,50 @@ module "keyvault" {
   service_principal_name      = var.service_principal_name
   service_principal_object_id = module.ServicePrincipal.service_principal_object_id
   service_principal_tenant_id = module.ServicePrincipal.service_principal_tenant_id
-
   depends_on = [
     module.ServicePrincipal
   ]
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_role_assignment" "pipeline_kv_secrets" {
+  scope                = module.keyvault.keyvault_id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+  depends_on = [
+    module.keyvault
+  ]
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = module.ServicePrincipal.client_id
+  value        = module.ServicePrincipal.client_secret
+  key_vault_id = module.keyvault.keyvault_id
+  depends_on = [
+    module.keyvault,
+    azurerm_role_assignment.pipeline_kv_secrets
+  ]
+}
+#create Azure Kubernetes Service
+module "aks" {
+  source                 = "../modules/aks/"
+  service_principal_name = var.service_principal_name
+  client_id              = module.ServicePrincipal.client_id
+  client_secret          = module.ServicePrincipal.client_secret
+  location               = var.location
+  resource_group_name    = var.rgname
+  cluster_name           = var.cluster_name
+  node_pool_name         = var.node_pool_name
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
+resource "local_file" "kubeconfig" {
+  depends_on   = [module.aks]
+  filename     = "./kubeconfig"
+  content      = module.aks.config
+}  ]
 }
 
 resource "azurerm_key_vault_secret" "example" {

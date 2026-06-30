ad492fc6dd2f6221e7354c32b5b0596721dc2825
Mehrunnisa <mehrunnisaafrah@gmail.com>
2026-06-30 16:50:02 +0530

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfdevbackend2024piyush"
-    container_name      = "tfstate"
-    key                 = "dev.tfstate"
+    storage_account_name = "tfdevafrah001"
+    container_name       = "tfstate"
+    key                  = "dev_tfstate_key"
   }
 }
 
